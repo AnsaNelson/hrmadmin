1276eed397ba460cd71d53a2f53f10bf3480f599
ansa nelson <ansanelson228@gmail.com>
2024-03-21 12:41:38 +0530

    register model

--- lib/app/modules/register/controllers/register_controller.dart
+++ lib/app/modules/register/controllers/register_controller.dart
@@ -1,7 +1,13 @@
+import 'package:email_validator/email_validator.dart';
+import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:motion_toast/motion_toast.dart';
 
 class RegisterController extends GetxController {
-  //TODO: Implement RegisterController
+  final GlobalKey<FormState> formKeys = GlobalKey<FormState>();
+  final TextEditingController nameController = TextEditingController();
+  final TextEditingController emailController = TextEditingController();
+  final TextEditingController passwordController = TextEditingController();
 
   final count = 0.obs;
   @override
@@ -18,6 +24,29 @@ class RegisterController extends GetxController {
   void onClose() {
     super.onClose();
   }
-
-  void increment() => count.value++;
+ void register(BuildContext context) {
+    String name=nameController.text.trim();
+    String email = emailController.text.trim();
+    String password = passwordController.text.trim();
+if(name.isEmpty){
+MotionToast.warning(
+        title: Text("Enter Name"),
+        description: Text("Please enter name"),
+      ).show(context);
+}
+    else if (email.isEmpty || !EmailValidator.validate(email)) {
+      MotionToast.warning(
+        title: Text("Enter Correct Email"),
+        description: Text("Please enter a valid email address."),
+      ).show(context);
+    } else if (password.isEmpty || password.length < 6 || !password.contains(RegExp(r'[A-Z]'))) {
+      MotionToast.warning(
+        title: Text("Enter Correct password"),
+        description: Text("Please enter at least more than 6 characters and also enter special characters."),
+      ).show(context);
+    } else {
+      // Your login logic here
+      Get.toNamed('/home');
+    }
+  }
 }
