1288b5d215bfe05fdae241c3ff905bc83e8b3f97
Eldho Paulose <eldhopaulose0485@gmail.com>
2024-03-22 14:30:29 +0530

    Update URL and register endpoint

--- lib/app/modules/register/views/register_view.dart
+++ lib/app/modules/register/views/register_view.dart
@@ -9,83 +9,87 @@ class RegisterView extends GetView<RegisterController> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      // appBar: AppBar(
-      //   title: const Text('RegisterView'),
-      //   centerTitle: true,
-      // ),
-      body: Padding(
-        padding: const EdgeInsets.only(top:100),
-        child: Container(
+        // appBar: AppBar(
+        //   title: const Text('RegisterView'),
+        //   centerTitle: true,
+        // ),
+        body: Padding(
+      padding: const EdgeInsets.only(top: 100),
+      child: Container(
           child: Form(
-            key: controller.formKeys,
-            child: Column(
-              children: [Text("Register ",style: TextStyle(fontSize: 20,fontWeight: FontWeight.bold),),
-              Padding(
-                padding: const EdgeInsets.all(10.0),
-                child: TextFormField(
-                  controller: controller.nameController,
-                  decoration: InputDecoration(
-                    border: OutlineInputBorder(borderRadius: BorderRadius.circular(20)),
-                    hintText: "Name"
-                  ),              
-                ),
-             
-              ),
-              SizedBox(height: 10,),
-               Padding(
-                padding: const EdgeInsets.all(10.0),
-                child: TextFormField(
-                  controller: controller.emailController,
-                  decoration: InputDecoration(
-                    border: OutlineInputBorder(borderRadius: BorderRadius.circular(20)),
-                    hintText: "Email"
-                  ),              
-                ),
-             
-              ),
-              SizedBox(height: 10,),
-              Padding(
-                padding: const EdgeInsets.all(10.0),
-                child: TextFormField(
-                  controller:controller.passwordController ,
-                  decoration: InputDecoration(
-                    border: OutlineInputBorder(borderRadius: BorderRadius.circular(20)),
-                    hintText: "password"
-                  ),              
-                ),
-             
-              ),
-              Container(
-                          height: 50,
-                          width: 100,
-                          decoration: BoxDecoration(
-                            borderRadius: BorderRadius.circular(10),
-                            gradient: LinearGradient(
-                              colors: [
-                                Color.fromRGBO(143, 148, 251, 1),
-                                Color.fromRGBO(143, 148, 251, .6),
-                              ],
-                            ),
-                          ),
-                          child: ElevatedButton(
-                            onPressed: () => controller.register(context),
-                            child: Text(
-                              "Submit",
-                              style: TextStyle(
-                                color: Colors.white,
-                                fontWeight: FontWeight.bold,
-                              ),
-                            ),
-                            style: ElevatedButton.styleFrom(
-                              primary: Colors.transparent,
-                              elevation: 0,
-                            ),
-                          ),
-                        ),
-              ],
+        key: controller.formKeys,
+        child: Column(
+          children: [
+            Text(
+              "Register ",
+              style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
             ),
-          )),
-      )
-    );
+            Padding(
+              padding: const EdgeInsets.all(10.0),
+              child: TextFormField(
+                controller: controller.nameController,
+                decoration: InputDecoration(
+                    border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(20)),
+                    hintText: "Name"),
+              ),
+            ),
+            SizedBox(
+              height: 10,
+            ),
+            Padding(
+              padding: const EdgeInsets.all(10.0),
+              child: TextFormField(
+                controller: controller.emailController,
+                decoration: InputDecoration(
+                    border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(20)),
+                    hintText: "Email"),
+              ),
+            ),
+            SizedBox(
+              height: 10,
+            ),
+            Padding(
+              padding: const EdgeInsets.all(10.0),
+              child: TextFormField(
+                controller: controller.passwordController,
+                decoration: InputDecoration(
+                    border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(20)),
+                    hintText: "password"),
+              ),
+            ),
+            Container(
+              height: 50,
+              width: 100,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(10),
+                gradient: LinearGradient(
+                  colors: [
+                    Color.fromRGBO(143, 148, 251, 1),
+                    Color.fromRGBO(143, 148, 251, .6),
+                  ],
+                ),
+              ),
+              child: ElevatedButton(
+                onPressed: () => controller.onRegisterClick(),
+                child: Text(
+                  "Submit",
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: Colors.transparent,
+                  elevation: 0,
+                ),
+              ),
+            ),
+          ],
+        ),
+      )),
+    ));
   }
 }
